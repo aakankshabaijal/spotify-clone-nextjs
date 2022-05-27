aa12a6d75a1cb5879f3e0cf950574e16f6849912
aakankshabaijal <f20190304@goa.bits-pilani.ac.in>
2022-05-27 13:30:19 +0530

    Finish Build

--- components/Player.js
+++ components/Player.js
@@ -3,9 +3,10 @@ import useSongInfo from '../hooks/useSongInfo';
 import { useSession } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { SwitchHorizontalIcon, HeartIcon, VolumeUpIcon as VolumeDownIcon } from '@heroicons/react/outline';
 import { RewindIcon, FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, VolumeUpIcon } from '@heroicons/react/solid';
+import { debounce } from 'lodash';
 
 function Player() {
 	const spotifyApi = useSpotify();
@@ -44,6 +45,13 @@ function Player() {
 		});
 	};
 
+	const debouncedAdjustVolume = useCallback(
+		debounce((volume) => {
+			spotifyApi.setVolume(volume).catch((err) => {});
+		}, 500),
+		[]
+	);
+
 	useEffect(
 		() => {
 			if (spotifyApi.getAccessToken() && !currentTrackId) {
@@ -53,6 +61,15 @@ function Player() {
 			}
 		},
 		[ currentTrackId, spotifyApi, session ]
+	);
+
+	useEffect(
+		() => {
+			if (volume > 0 && volume < 100) {
+				debouncedAdjustVolume(volume);
+			}
+		},
+		[ volume ]
 	);
 
 	let songImg;
@@ -99,6 +116,27 @@ function Player() {
 					onClick={() => spotifyApi.skipToNext()}
 				/>
 				<ReplyIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+			</div>
+
+			{/* Right */}
+
+			<div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+				<VolumeDownIcon
+					className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+					onClick={() => volume > 0 && setVolume(volume - 10)}
+				/>
+				<input
+					type="range"
+					value={volume}
+					min={0}
+					max={100}
+					className="w-14 md:w-28"
+					onChange={(e) => setVolume(Number(e.target.value))}
+				/>
+				<VolumeUpIcon
+					className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+					onClick={() => volume < 100 && setVolume(volume + 10)}
+				/>
 			</div>
 		</div>
 	);
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getProviders, signIn } from 'next-auth/react';
+import { HeartIcon } from '@heroicons/react/solid';
 
 function Login({ providers }) {
 	return (
@@ -15,6 +16,16 @@ function Login({ providers }) {
 					</button>
 				</div>
 			))}
+			<p className="text-white mt-5">
+				Open Spotify on any of your devices to view your playlists and control playback from this clone
+			</p>
+
+			<p className="text-white flex-end absolute bottom-5 mb-2">
+				Made with <HeartIcon className="w-5 h-5 inline " /> by{' '}
+				<a href="https://www.linkedin.com/in/aakankshabaijal/" className="text-white">
+					Aakanksha Baijal
+				</a>
+			</p>
 		</div>
 	);
 }
